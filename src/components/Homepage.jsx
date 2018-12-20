de3474946349974d11384dc914a22eae6651f907
Steven AD <s.dailliez@gmail.com>
2018-12-20 22:25:21 +0100

    Add alt attributs to images.

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { TwitterTimelineEmbed } from "react-twitter-embed";
+import React from 'react';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
 
 class Homepage extends React.Component {
   constructor(props) {
@@ -8,24 +8,28 @@ class Homepage extends React.Component {
   }
   render() {
     return (
-      <div container-fluid="true">
-        <img className="img-fluid bg-img" src="./bg-Homepage.jpg" alt="cook" />
-        <div className="row text-center">
-          <div className="col-lg-3 pt-5">
-            <p className="approach">Chiffres-clés</p>
+      <div container-fluid='true'>
+        <img
+          className='img-fluid bg-img'
+          src='./bg-Homepage.jpg'
+          alt='background'
+        />
+        <div className='row text-center'>
+          <div className='col-lg-3 pt-5'>
+            <p className='approach'>Chiffres-clés</p>
             <p>Le smartphone c'est aussi :</p>
-            <ul id="keynumbers">
+            <ul id='keynumbers'>
               <li>11% de la consommation énergétique mondiale</li>
               <li>3% des émissions carbone de la planète</li>
               <li>22% de notre pollution quotidienne</li>
             </ul>
-            <p className="knowledge">Le saviez-vous?</p>
+            <p className='knowledge'>Le saviez-vous?</p>
             <p>
               Recycler un smartphone permet d’économiser 70 kg de ressources
               naturelles
             </p>
           </div>
-          <div className="col-lg-6 pt-5">
+          <div className='col-lg-6 pt-5'>
             <p>
               On oublie trop souvent que la transition numérique, engagée depuis
               plusieurs années, a une empreinte environnementale colossale.
@@ -35,7 +39,7 @@ class Homepage extends React.Component {
               différents matériaux qui polluent, chacun à sa manière.
             </p>
             <br />
-            <p className="approach">Notre démarche ?</p>
+            <p className='approach'>Notre démarche ?</p>
             <br />
             <p>
               Le Smartphone est l’outil que vous avez toujours dans la poche, à
@@ -49,15 +53,15 @@ class Homepage extends React.Component {
               durables sur le marché actuel !
             </p>
             <br />
-            <button id="quizButton" className="mb-4 mt-1">
+            <button id='quizButton' className='mb-4 mt-1'>
               Accède au quiz
             </button>
           </div>
-          <div className="col-lg-3 pt-5">
+          <div className='col-lg-3 pt-5'>
             <div>
               <TwitterTimelineEmbed
-                sourceType="profile"
-                screenName="green_sober"
+                sourceType='profile'
+                screenName='green_sober'
                 options={{ height: 800, width: 400 }}
               />
             </div>
