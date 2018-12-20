c6e1bba5d1c3dc4135bb77c540bd2b870e10f921
Emma Kimpe <emmaemobaka@gmail.com>
2018-12-20 22:37:21 +0100

    Adding files to Blog.jsx.

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -26,7 +26,7 @@ const Example = props => {
                   Développement durable et numérique : un impact qui n’est pas
                   virtuel
                 </CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardSubtitle> </CardSubtitle>
                 <CardText>
                   Bien que les révolutions technologiques puissent être perçus
                   comme des « alliés » du développement durable, il faut
@@ -49,7 +49,7 @@ const Example = props => {
                 <CardTitle>
                   Le terrible impact des smartphones sur l’environnement
                 </CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardSubtitle> </CardSubtitle>
                 <CardText>
                   Alors qu’il se vend près de 47 smartphones par seconde dans le
                   monde, cette production n’est pas sans conséquence sur la
@@ -66,12 +66,13 @@ const Example = props => {
                 alt="Card image cap"
               />
               <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardTitle>Le terrible bilan carbone des smartphones</CardTitle>
+                <CardSubtitle> </CardSubtitle>
                 <CardText>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                  Les émissions de gaz à effet de serre (GES) des technologies
+                  de l’information et de la communication ne cessent de
+                  progresser, avec une mention toute particulière pour les
+                  smartphones.
                 </CardText>
                 <Button>Button</Button>
               </CardBody>
@@ -85,12 +86,16 @@ const Example = props => {
             <Card>
               <CardImg top width="100%" src="./nope.jpg" alt="Card image cap" />
               <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardTitle>Numérique : le grand gâchis énergétique</CardTitle>
+                <CardSubtitle> </CardSubtitle>
                 <CardText>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                  Ordinateurs, data centers, réseaux… engloutissent près de 10 %
+                  de la consommation mondiale d’électricité. Et ce chiffre ne
+                  cesse d’augmenter. S’il n’est évidemment pas question de se
+                  passer des progrès apportés par le numérique, les
+                  scientifiques pointent un mode de fonctionnement peu optimisé
+                  et très énergivore. Cet article fait partie du Top 10 des
+                  contenus les plus lus sur notre site cette année.
                 </CardText>
                 <Button>Button</Button>
               </CardBody>
@@ -103,12 +108,14 @@ const Example = props => {
                 alt="Card image cap"
               />
               <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardTitle>Le numérique, c’est plus écologique ?</CardTitle>
+                <CardSubtitle> </CardSubtitle>
                 <CardText>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                  Abandonner les supports matériels et choisir le numérique,
+                  est-ce la solution pour économiser les ressources naturelles ?
+                  La réponse n’est pas évidente : si le papier consomme des
+                  arbres, l’électronique utilise des métaux, du plastique, des
+                  terres rares… Décryptage au cas par cas.
                 </CardText>
                 <Button>Button</Button>
               </CardBody>
@@ -116,12 +123,14 @@ const Example = props => {
             <Card>
               <CardImg top width="100%" src="./tree.jpg" alt="Card image cap" />
               <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardTitle>
+                  Fairphone, l’OVNI des smartphones, à l’assaut du grand public
+                </CardTitle>
+                <CardSubtitle> </CardSubtitle>
                 <CardText>
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                  Fairphone 2, un smartphone respectueux de l'environnement et
+                  des personnes qui le fabriquent, est désormais distribué en
+                  France via l'opérateur Orange. Conçu pour
                 </CardText>
                 <Button>Button</Button>
               </CardBody>
