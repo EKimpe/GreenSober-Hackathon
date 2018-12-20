5eea300531fc650cafea1f47bf3eee021a900943
edelattre <elie1delattre@gmail.com>
2018-12-20 16:57:15 +0000

    Modifying App.js.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 import "./App.css";
-import Accueil from "./components/Accueil";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Homepage from "./components/Homepage";
 import Quiz from "./components/Quiz";
 import Blog from "./components/Blog";
-import Twitter from "./components/Twitter";
-=======
-import React, { Component } from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Homepage from './components/Homepage';
-import Quiz from './components/Quiz';
-import Blog from './components/Blog';
->>>>>>> 03e1e408ef4d88ffc52cc8a62ab8b5e0ebcffbf1
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <Header />
         <BrowserRouter>
           <Switch>
-<<<<<<< HEAD
-            <Route exact path="/" component={Accueil} />
+            <Route exact path="/" component={Homepage} />
             <Route path="/quiz" component={Quiz} />
             <Route path="/blog" component={Blog} />
-            <Route path="/twitter" component={Twitter} />
-=======
-            <Route exact path='/' component={Homepage} />
-            <Route path='/quiz' component={Quiz} />
-            <Route path='/blog' component={Blog} />
->>>>>>> 03e1e408ef4d88ffc52cc8a62ab8b5e0ebcffbf1
           </Switch>
         </BrowserRouter>
         <Footer />
