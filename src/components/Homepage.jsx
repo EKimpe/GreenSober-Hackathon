06bf75fb4e768f39b56e47d869f7eaa782adb009
Steven AD <s.dailliez@gmail.com>
2018-12-20 19:09:13 +0100

    Add background image and prepare the content display.

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,8 +7,34 @@ class Homepage extends React.Component {
   }
   render() {
     return (
-      <div>
-        <p>toto</p>
+      <div container-fluid>
+        <div container-fluid id='bgHomepage'>
+          <img id='backgroundImg' src='./bg-Homepage.jpg' alt='background' />
+        </div>
+        <div className='row justify-content-center'>
+          <div className='col-lg-3 col-md-12'>
+            <p>Chiffres-clés</p>
+            <br />
+            <p className='knowledge'>Le saviez-vous?</p>
+          </div>
+          <div className='col-lg-5 col-md-12'>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+              impedit vero deleniti pariatur necessitatibus laborum accusamus
+              at, minima rem. Dolorum officiis, praesentium eius quis voluptatum
+              eaque adipisci culpa ex consequatur.Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Quasi impedit vero deleniti pariatur
+              necessitatibus laborum accusamus at, minima rem. Dolorum officiis,
+              praesentium eius quis voluptatum eaque adipisci culpa ex
+              consequatur.
+            </p>
+            <br />
+            <button id='quizButton'>Accèdes au quiz</button>
+          </div>
+          <div className='col-lg-3 col-md-12'>
+            <div id='lastTweet'>Derniers tweets</div>
+          </div>
+        </div>
       </div>
     );
   }
