51872fdb043fc386f2ffa0ab37e03579d83ef981
Steven AD <s.dailliez@gmail.com>
2018-12-20 15:16:17 +0100

    Create Header & Footer components.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from "react";
-import { Route, BrowserRouter, Switch } from "react-router-dom";
-import "./App.css";
-import Accueil from "./components/Accueil";
-import Quiz from "./components/Quiz";
-import Blog from "./components/Blog";
+import React, { Component } from 'react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Homepage from './components/Homepage';
+import Quiz from './components/Quiz';
+import Blog from './components/Blog';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
+        <Header />
         <BrowserRouter>
           <Switch>
-            <Route exact path="/" component={Accueil} />
-            <Route path="/quiz" component={Quiz} />
-            <Route path="/blog" component={Blog} />
+            <Route exact path='/' component={Homepage} />
+            <Route path='/quiz' component={Quiz} />
+            <Route path='/blog' component={Blog} />
           </Switch>
         </BrowserRouter>
+        <Footer />
       </div>
     );
   }
