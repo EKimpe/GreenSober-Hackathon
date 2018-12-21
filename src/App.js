0262eb9ad0fe6faad42e7f3b5c3b3a1c747e3c06
Steven AD <s.dailliez@gmail.com>
2018-12-21 08:22:24 +0100

    Create Contact component and add the route.

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
             <Route exact path='/' component={Homepage} />
             <Route path='/quiz' component={Quiz} />
             <Route path='/blog' component={Blog} />
+            <Route path='/contact' component={Contact} />
           </Switch>
         </BrowserRouter>
         <Footer />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -50,7 +50,7 @@ class Header extends React.Component {
                 </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink id='nav-item' className='pl-5 ml-5' href='/blog'>
+                <NavLink id='nav-item' className='pl-5 ml-5' href='/contact'>
                   Nous contacter
                 </NavLink>
               </NavItem>
