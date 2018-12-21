76a0a353b032f7a04803549deb500a0f8ba215f3
Steven AD <s.dailliez@gmail.com>
2018-12-21 10:51:47 +0100

    Create BlogDetails component. Make link with Blog component thanks to onClick.

---
+++ src/components/BlogDetails.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const BlogDetails = props => {
+  return (
+    <div>
+      <img className='float' src='./img/city.jpg' alt='City' />
+      <p>
+        Devenu un objet dont on peut difficilement se passer, le smartphone voit
+        ses ventes croître de manière fulgurante depuis plus d'une décennie. En
+        2017, près de 1.5 milliard de smartphones ont ainsi été vendus, soit
+        2,7% de plus qu'en 2016 (1). D'ici quelques années, le nombre de
+        smartphones pourrait même dépasser celui des ordinateurs de bureau et
+        des ordinateurs portables.
+      </p>
+      <br />
+      <p>
+        Malheureusement, cette production et consommation de smartphones a des
+        conséquences néfastes sur la planète. Quels sont exactement les impacts
+        d'un smartphone sur l'environnement ? Que peut faire le consommateur
+        pour limiter les dégâts ?
+      </p>
+      <br />
+      <p>
+        <strong>L'impact du smartphone sur le dérèglement climatique</strong>
+      </p>
+      <br />
+      <p>
+        Si les ventes de téléphones portables par millions à l'échelle mondiale
+        ont de quoi satisfaire les opérateurs, notre planète, elle, se désole.
+        Car contrairement à leur impact sur la santé qui divise encore les
+        scientifiques, l'impact des smartphones sur l'environnement ne laisse
+        plus de place à l'interprétation. Malgré leur petite dimension, les
+        téléphones portables dernier cri sont de gros consommateurs d'énergie et
+        de matériaux rares ou toxiques.
+      </p>
+    </div>
+  );
+};
+
+export default BlogDetails;
